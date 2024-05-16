18a8cc891090d050a39987c2ea14bca62e6f7f1c
kentem-sh-maehata <sh-maehata@kentem@.co.jp>
2024-05-16 11:52:18 +0900

    Startボタンの無効化

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 {
-    var btn = document.getElementById('btn');
+    var btn_1 = document.getElementById('btn');
     var p_1 = document.querySelector('p');
-    btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', function () {
+    btn_1 === null || btn_1 === void 0 ? void 0 : btn_1.addEventListener('click', function () {
+        if (p_1 === null || p_1 === void 0 ? void 0 : p_1.textContent)
+            p_1.textContent = "00:03";
+        btn_1.disabled = true;
+        btn_1.classList.add('enabled');
         var endTime = new Date().getTime() + 3000;
-        console.log(new Date().getTime());
-        console.log(endTime);
         var setinterval = setInterval(function () {
             var remain = endTime - new Date().getTime();
             console.log(remain);
@@ -20,6 +22,8 @@
                 if (p_1 === null || p_1 === void 0 ? void 0 : p_1.textContent)
                     p_1.textContent = "00:00";
                 clearInterval(setinterval);
+                btn_1.classList.remove('enabled');
+                btn_1.disabled = false;
             }
         }, 100);
     });
--- main.ts
+++ main.ts
@@ -3,9 +3,11 @@
   const p = document.querySelector('p')
 
   btn?.addEventListener('click',()=>{
+    if(p?.textContent) p.textContent = "00:03"
+    btn.disabled = true
+    btn.classList.add('enabled')
     const endTime:number = new Date().getTime() + 3000
-    console.log(new Date().getTime())
-    console.log(endTime)
+    
     const setinterval = setInterval(()=>{
       var remain = endTime - new Date().getTime()
       console.log(remain)
@@ -18,6 +20,8 @@
       if(remain < 0){
         if(p?.textContent) p.textContent = "00:00"
         clearInterval(setinterval)
+        btn.classList.remove('enabled')
+        btn.disabled = false
       }
     },100)
   })
